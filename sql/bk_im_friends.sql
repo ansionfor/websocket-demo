a3d52ea4c5db486fe558742f8f77bbf17054f1b7
heansheng <77931774@qq.com>
2025-02-02 23:52:26 +0800

    Update bk_im_friends.sql

--- sql/bk_im_friends.sql
+++ sql/bk_im_friends.sql
@@ -4,4 +4,4 @@ CREATE TABLE `bk_im_friends` (
  `friends_ids` varchar(1024) NOT NULL COMMENT '好友id列表，[id,id]',
  PRIMARY KEY (`id`),
  UNIQUE KEY `publisher_id` (`publisher_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
